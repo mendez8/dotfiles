263d4dd91b1014bc40192266c56f35914a49f6e2
Miguel <mendez8@gmail.com>
2023-09-06 13:49:42 +0200

    Update Brewfile and sketchybar

--- osx/sketchybar/plugins/wifi.sh
+++ osx/sketchybar/plugins/wifi.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# The wifi_change event supplies a $INFO variable in which the current SSID
-# is passed to the script.
+CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
-WIFI=${INFO:-"Not Connected"}
-
-sketchybar --set $NAME label="${WIFI}"
+if [ "$SSID" = "" ]; then
+  sketchybar --set $NAME label="Disconnected" icon=󰤮
+else
+  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=󰖩
+fi
